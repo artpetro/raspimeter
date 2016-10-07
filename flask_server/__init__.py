1e193fdbb61c25e9a8057566d3a6bd283e1f8fc2
artpetro <artpetro@uos.de>
2016-10-08 00:58:59 +0200

    db name changed

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 
-app.config["MONGODB_SETTINGS"] = {'DB': "raspimeter_pi"}
+app.config["MONGODB_SETTINGS"] = {'DB': "raspimeter"}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 mongo_db_engine = MongoEngine(app)
