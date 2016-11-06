174995d5e20ddefb0b62e71edc69a6574af1273e
artpetro <artpetro@uos.de>
2016-11-06 23:15:40 +0100

    js image ref

--- flask_server/static/js/raspimeter.js
+++ flask_server/static/js/raspimeter.js
@@ -789,13 +789,13 @@ $(function () {
 	/* refresh imeges in settings view */
 	setInterval(function(){
 		var img = $("#last_meter_capture");
-		var src = img.attr("src");
+		var src = img.attr("src").split('?')[0];
 	    img.attr("src", src + "?"+new Date().getTime());
 	},3000);
 	
 	setInterval(function(){
 		var img = $("#last_knn_capture");
-		var src = img.attr("src");
+		var src = img.attr("src").split('?')[0];
 	    img.attr("src", src + "?"+new Date().getTime());
 	},3000);
 });
