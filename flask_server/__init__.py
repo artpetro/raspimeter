41bfe984ce65baff3fdf7c3c91e47305eb1dff56
artpetro <artpetro@uos.de>
2016-11-07 16:00:40 +0100

    config reading disabled

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
 import ConfigParser
+import os
 
 app = Flask(__name__)
 mongo_db_engine = None
@@ -12,25 +13,25 @@ def initMongoEngine(name, password):
     app.config["MONGODB_SETTINGS"] = {'DB': name}
     app.config["SECRET_KEY"] = password
     mongo_db_engine =  MongoEngine(app)
-    
+
 def updateDBConfig(name, password):
     config = ConfigParser.ConfigParser()
     config.read('../db/config.cfg')
     config.set('Database', 'name', name)
     config.set('Database', 'password', password)
-        
+
     with open('../db/config.cfg', 'wb') as configfile:
         config.write(configfile)
-        
-    initMongoEngine(name, password)
-    
-    
 
-config = ConfigParser.ConfigParser()
-config.read('../db/config.cfg')
-    
-db_name = config.get('Database', 'name')
-password = config.get('Database', 'password')
+    initMongoEngine(name, password)
+
+
+
+#config = ConfigParser.ConfigParser()
+#config.read('db/config.cfg')
+
+db_name = 'raspimeter'#config.get('Database', 'name')
+password = 'password'#config.get('Database', 'password')
 
 initMongoEngine(db_name, password)
     
