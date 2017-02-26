68d3be2c988691b971dc46421a8e6ac80e963a7c
Artem Petrov <artpetro@uos.de>
2017-02-26 06:30:34 +0100

    db backup changed

--- db/db_backup.py
+++ db/db_backup.py
@@ -1,23 +1,26 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask_server import app
 import time
+import traceback
+from subprocess import call
 
 backup_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'flask_server', 'static', 'db_backups'))
 
 def backup():
     #make backup to tmp dir raspimeter_db_timestamp
-    tmp_dir = os.path.abspath(os.path.join(backup_dir, 'raspimeter_db_%s' % int(round(time.time()))))
+    tmp_dir = 'raspimeter_db_%s' % int(round(time.time()))
+    tmp_path = os.path.abspath(os.path.join(backup_dir, tmp_dir))
+    archive_path = os.path.abspath(os.path.join(backup_dir, '%s.tar.gz' % tmp_dir))
     try:
-        call(['mongodump', '--out', tmp_dir])
-        #make tar.gz
-        #remove tmp dir
-        print 'OK'
+        call(['mongodump', '--out', tmp_path])
+        call(['tar', '-zcvf', archive_path, tmp_path])
+        call(['rm', '-rf', tmp_path])
+        return True
      
     except Exception as e:
-        message = "ERROR"
         traceback.print_exc()
+	return False
     
 def restore():
     pass
